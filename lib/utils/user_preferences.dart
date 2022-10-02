b5bb320efc411a2691b263b18253a534be48f0ca
Kritish Pokharel <kritish.pokharel@icloud.com>
2022-10-02 11:39:00 -0400

    File Upload for hackUMBC

---
+++ lib/utils/user_preferences.dart
@@ -0,0 +1,15 @@
+
+
+import 'package:fluttercookie/model/user.dart';
+
+class UserPreferences {
+  static const myUser = User(
+    imagePath:
+        'https://scontent.cdninstagram.com/v/t1.15752-9/308516768_497805675588372_1181227456641022689_n.jpg?stp=dst-jpg_p600x600&_nc_cat=104&ccb=1-7&_nc_sid=5a057b&_nc_ohc=R6ZmDJ1nHzgAX_QsroK&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.cdninstagram.com&oh=03_AVK2JN4MS4tYSaWWmq06AhLsL_7x4KmlA-j0T5Bj4YX-mA&oe=635EE73C',
+    name: 'Ankur Gyawali',
+    email: 'ankurgyawali123@gmail.com',
+    about:
+        'Sexy at Howard University. Freshman: Computer Science. Love girls especially my girls at Howard and HU you know. I am a play boy so girl stay away',
+    isDarkMode: false,
+  );
+}
